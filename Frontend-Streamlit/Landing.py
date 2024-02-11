eaeeadd2f07c2843c925af50d3e14f77eb4d0d91
Ojaswi Chopra <99067527+ojaswichopra@users.noreply.github.com>
2024-02-11 23:10:59 +0530

    Create Landing.py

---
+++ Frontend-Streamlit/Landing.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# Settings
+page_title = "Hara Bhara Nivesh"
+page_icon = ":money_with_wings:"  # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
+layout = "centered"
+
+st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
+
+st.title("Landing Page")
