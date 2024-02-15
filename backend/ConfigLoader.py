42828e34963b27f60c39e196b66089f4e80860d4
vidishaAG <lci2021018@iiitl.ac.in>
2024-02-16 01:10:49 +0530

    Set up database connection

---
+++ backend/ConfigLoader.py
@@ -0,0 +1,19 @@
+from configparser import ConfigParser
+
+class ConfigLoader:
+    def __init__(self, config_file_path):
+        self.config = ConfigParser()
+        self.config.read(config_file_path)
+
+    def get_host(self):
+        return self.config['DATABASE']['host']
+
+    def get_user(self):
+        return self.config['DATABASE']['user']
+
+    def get_password(self):
+        return self.config['DATABASE']['password']
+
+    def get_database(self):
+        return self.config['DATABASE']['database']
+ 
