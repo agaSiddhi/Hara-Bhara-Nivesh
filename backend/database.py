68d93211fdc496c57b0e8b050f42362a7823dec9
Ojaswi Chopra <ojaswichopra06@gmail.com>
2024-02-17 02:11:30 +0530

    frontend

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 def read_company_data():
-    company_data=pd.read_csv("/Users/vidisha/Desktop/DESISProject/DesisSG-2/backend/ListOfCompanies.csv")
+    company_data=pd.read_csv("/Users/ojaswichopra/Downloads/DESIS/project/DesisSG-2/backend/ListOfCompanies.csv")
     json_data= company_data.to_json( orient='records')
     return json_data
 
