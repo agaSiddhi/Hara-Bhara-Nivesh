9fe729c635e793aff792f395e5f5e407dd50d0c5
Ojaswi Chopra <ojaswichopra06@gmail.com>
2024-02-17 02:05:32 +0530

    frontendChange

--- Frontend-Streamlit/Landing.py
+++ Frontend-Streamlit/Landing.py
@@ -1,10 +1,23 @@
 import streamlit as st
-
-# Settings
 page_title = "Hara Bhara Nivesh"
 page_icon = ":money_with_wings:"  # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
 layout = "centered"
 
-st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
+st.set_page_config(initial_sidebar_state="collapsed",page_title=page_title, page_icon=page_icon, layout=layout)
+
+st.markdown(
+    """
+<style>
+    [data-testid="collapsedControl"] {
+        display: none
+    }
+</style>
+""",
+    unsafe_allow_html=True,
+)
+# Settings
+
 
 st.title("Landing Page")
+if st.button("Companies List"):
+    st.switch_page("pages/1_Listings.py")
