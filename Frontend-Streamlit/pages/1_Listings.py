1c3a08c216e7ee5460cba61244c05b4c49cb8ee0
Ojaswi Chopra <99067527+ojaswichopra@users.noreply.github.com>
2024-02-17 01:58:03 +0530

    Update 1_Listings.py

--- Frontend-Streamlit/pages/1_Listings.py
+++ Frontend-Streamlit/pages/1_Listings.py
@@ -1,17 +1,20 @@
 import json
 import streamlit as st
 from backend.database import read_company_data
+# from pages.details_page import show_details_page
+
 # Define a list of companies
 companies = json.loads(read_company_data())
 
-
 # Create a Streamlit app
 def main():
+    if st.button("Back to Home"):
+        st.switch_page("Landing.py")
     # Page title
     st.title("Top Sustainable Companies")
 
     # Search bar
-    search_term = st.text_input("",placeholder= "Enter Company Name")
+    search_term = st.text_input("", placeholder="Enter Company Name")
     search_button = st.button("Search")
 
     # Filter companies based on search term
@@ -19,10 +22,19 @@ def main():
 
     # Display filtered companies
     for company in filtered_companies:
-        st.subheader(f"{company['Rank']}. {company['Name']}")
-        col1, col2 = st.columns([3,1])
-        col1.write(company["Description"])
-        col2.write(get_stars_html(company["Rating"]))
+        company_name = f"{company['companyID']}. {company['Name']}"
+        st.subheader(company_name)
+        col1, col2, col3 = st.columns([3, 1, 1])
+        col1.write(company["description"])
+        col2.write(get_stars_html(company["rating"]))
+        
+        # Add detail button to view company details
+        button_label = "Details"
+        button_key = f"{button_label}_{company_name}"
+        if col3.button(button_label, key=button_key):
+            st.session_state['company'] = company
+            st.switch_page("pages/details_page.py")
+                # show_details_page(company)  # Pass the 'company' details as an argument
 
 # Function to generate HTML code for star rating
 def get_stars_html(rating):
