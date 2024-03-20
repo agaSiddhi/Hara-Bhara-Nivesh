dc2eff0149d74aeb5cb3811f7db929426ae9fa67
Siddhi Agarwal <siddhiag@iitbhilai.ac.in>
2024-03-20 19:34:25 +0530

    added funcs in credit details page

--- Frontend-Streamlit/pages/cer_details_page.py
+++ Frontend-Streamlit/pages/cer_details_page.py
@@ -2,31 +2,29 @@ import streamlit as st
 import pandas as pd
 import os
 
+from backend.configuration import initialize_system
+company_service = initialize_system()[0]  
+
 def append(company,user_bid):
     company['Bids'].append({'Bidder': st.session_state.bidder, 'Bid': user_bid})
     st.success(f"Bid placed successfully! Current highest bid: ${user_bid} by {st.session_state.bidder}")
 
 def get_current_price(company_ticker):
-    your_listing_file_path = "your_listing_file.csv"  # Replace with the actual path
-    your_listing_df = pd.read_csv(your_listing_file_path)
 
-    company_data = your_listing_df[your_listing_df['company_ticker'] == company_ticker]
-    if not company_data.empty:
-        return company_data['initial_bid'].values[0]
+    listings = company_service.return_listings_for_auction()
+    df1 = pd.DataFrame(listings, columns=["bidID", "initial_Bid", "minimum_Step", "credits_Listed", "companyID"])
+    ini_bid = max(df1[df1['companyID'] == company_ticker]['initial_Bid'])
+    return ini_bid 
+
+def get_maximum_bidding_price(company_ticker,company_details):
+    max_amount = company_service.return_max_bidding_amount(company_ticker)[0][0]
+    print("max bid price",max_amount)
+    if max_amount:
+        return max_amount
     else:
-        return 0 
-
-def get_maximum_bidding_price(company_ticker):
-    bidding_list_file_path = "bidding_list.csv"  # Replace with the actual path
-    if not os.path.exists(bidding_list_file_path):
-        return get_current_price(company_ticker) 
-    bidding_list_df = pd.read_csv(bidding_list_file_path)
-
-    company_bids = bidding_list_df[bidding_list_df['company_ticker'] == company_ticker]
-    if not company_bids.empty:
-        return company_bids['bid_amount'].max()
-    else:
-        return get_current_price(company_ticker)  # Return the initial bid if no bids are placed
+        return get_current_price(company_details.get('companyID'))
+        # print("ini bid",ini_bid)
+        # return ini_bid  # Return the initial bid if no bids are placed
 
 
 def main():
@@ -39,9 +37,11 @@ def main():
 
         # Display company details
         # st.subheader(f"Company Name: {company_details.get('Name', 'N/A')}")
-        st.subheader(f"{company_details.get('company_ticker', 'N/A')}")
-        st.write(f"Initial Bidding Price: ${get_current_price(company_details['company_ticker'])}")
-        st.write(f"Current highest bid: ${get_maximum_bidding_price(company_details['company_ticker'])}")
+        st.subheader(f"{company_details.get('companyID', 'N/A')}")
+        # st.write(f"Initial Bidding Price: ${get_current_price(company_details['companyID'])}")
+        st.write(f"Initial Bidding Price: $ {company_details.get('initial_Bid',0)}")
+
+        st.write(f"Current highest bid: $ {get_maximum_bidding_price(company_details['companyID'],company_details)}")
 
         col1,col2 =st.columns(2)
         # Step 1: Click "Place Bid" button to initiate the bid placement
